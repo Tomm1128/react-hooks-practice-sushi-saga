f3825f1a7fe0e630bd03fb3dcd5133e65821b338
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-24 10:09:30 -0400

    Add to wallet button

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,16 @@
 import React from "react"
 
-function Table({ plates = [], moneySpent }) {
-  const money = 100
-
+function Table({ plates = [], sushiWallet, updateSushiWallet, moneySpent }) {
   const emptyPlates = plates.map((_, index) => (
     <div key={index} className="empty-plate" style={{ top: -7 * index }} />
   ))
 
   return (
     <>
-      <h1 className="remaining">You have: ${money - moneySpent} remaining!</h1>
+      <h1 className="remaining">
+        You have: ${sushiWallet - moneySpent} remaining!
+      </h1>
+      <button className="wallet-button">Add More to Wallet</button>
       <div className="table">
         <div className="stack">{emptyPlates}</div>
       </div>
