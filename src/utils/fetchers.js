e5d338334476979f31966aef897aa188c20f9271
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-24 09:51:26 -0400

    Remove unused fetch function

--- src/utils/fetchers.js
+++ src/utils/fetchers.js
@@ -4,15 +4,4 @@ const getSushi = () => {
   return fetch(API).then((resp) => resp.json())
 }
 
-const updateSushi = (sushi) => {
-  return fetch(API, {
-    method: "PATCH",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(sushi),
-  })
-}
-
-export { getSushi, updateSushi }
+export { getSushi }
